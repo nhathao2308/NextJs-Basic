328cb2fdf8d94abef9799a3d1cdcfe934c2af244
NguyenNhatHao <nhathao230803@gmail.com>
2025-06-20 17:50:15 +0700

    setting

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center">
+        <h1 className="text-2xl font-bold">Login Page</h1>
+      </div>
+    </div>
+  );
+}
